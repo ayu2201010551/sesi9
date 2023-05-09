3588e5063431287a9d4b37303be08dda0ee0513c
Ni Luh Nyoman Ayu Menuh <127836672+ayu2201010551@users.noreply.github.com>
2023-05-09 17:03:30 +0800

    project sesi9

---
+++ javascript03.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 3
+    </title>
+</head>
+<body>
+    
+    <div id="tampil"></div>
+    <script>
+        var bill1 = 3;
+        let bill2 = 7;
+        var hasil = bill1 + bill2 ;
+
+        document.getElementById("tampil").innerHTML = " Hasil Penjumlahan "+ bill1+" dengan "+bill2+" adalah "+hasil;
+    </script>
+
+</body>
+</html>
